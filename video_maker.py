bb506cd6d141fa21427e4cbabe8f203c4ab21f45
Chengyuan Qian <cyqian@tamu.edu>
2022-04-08 15:01:44 -0500

    debug video maker

--- video_maker.py
+++ video_maker.py
@@ -1,23 +1,23 @@
-    import cv2
-    import argparse
-    import os
+import cv2
+import argparse
+import os
 
-    def video_maker(image_folder,video_name = 'video.avi'):
-        images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-        frame = cv2.imread(os.path.join(image_folder, images[0]))
-        height, width, layers = frame.shape
+def video_maker(image_folder,video_name = 'video.avi'):
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
 
-        video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
-        for image in images:
-            video.write(cv2.imread(os.path.join(image_folder, image)))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
 
-        cv2.destroyAllWindows()
-        video.release()
+    cv2.destroyAllWindows()
+    video.release()
 
-    if __name__=='__main__':
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--image-dir', type=str, default='/dataset/semantic-kitti/00/velodyne/outputs/')
-        args = parser.parse_args()
-        video_maker(args.image_dir)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image-dir', type=str, default='/dataset/semantic-kitti/00/velodyne/outputs/')
+    args = parser.parse_args()
+    video_maker(args.image_dir)
              
