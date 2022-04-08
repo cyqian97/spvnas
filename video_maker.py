c9504ffa371518601f5f8535c2cc44707738fd2b
Chengyuan Qian <cyqian@tamu.edu>
2022-04-08 15:20:34 -0500

    tested video maker

--- video_maker.py
+++ video_maker.py
@@ -2,13 +2,15 @@ import cv2
 import argparse
 import os
 
-def video_maker(image_folder,video_name = 'video.avi'):
+def video_maker(image_folder,video_name = 'video.mp4'):
     images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    images.sort()
+    # print(images)
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
-
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video = cv2.VideoWriter(video_name, fourcc, 10, (width,height))
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
 
