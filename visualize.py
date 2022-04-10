aaa2b5388754b430dec96e4190763d9445b46b59
Chengyuan Qian <cyqian@tamu.edu>
2022-04-10 16:54:07 -0500

    modify mlab.view paremeters

--- visualize.py
+++ visualize.py
@@ -191,7 +191,7 @@ def draw_lidar(pc,
     pts.module_manager.scalar_lut_manager.lut.number_of_colors = cmap.shape[0]
 
     mlab.view(azimuth=270,
-              elevation=90,
+              elevation=0,
               focalpoint= [0, 13, 1],
               distance=62,
               figure=fig)
